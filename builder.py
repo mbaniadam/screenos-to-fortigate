de1bfa9b6bb6ec918b62e1a5216975f3668158e9
mbaniadam <mortalbm@gmail.com>
2023-10-04 18:36:03 +0330

    address and address groups

--- builder.py
+++ builder.py
@@ -23,42 +23,53 @@ def define_ports(port_data):
         return port
 
 
-def define_addr(address_name, subnet,converted_addr):
-    converted_addr.write(f"edit {address_name}\n")
-    converted_addr.write(f"set subnet {subnet}\n")
-    converted_addr.write(f"set comment {description}\n")
-    converted_addr.write("next\n")
+def define_addr(address_data):
+    
+    for address_name, address_value in address_data.items():
+        if isinstance(address_value, list):
+            # If the value is a list, assume it's a subnet and add it to the addresses
+            subnet = ''.join(address_data.get(address_name))
+            converted_config.write(f"edit {address_name}\n")
+            converted_config.write(f"set subnet {subnet}\n")
+            converted_config.write("next\n")
+        elif isinstance(address_value, dict):
+            # If the value is a dictionary, recursively extract addresses from it
+            grp_name = address_name
+            grp_memebers = address_value
+            groups.update({grp_name:grp_memebers})
 
 
-def define_addrgrp(grp_name,address_name,converted_grp):
-    converted_grp.write(f"edit {grp_name}\n")
-    converted_grp.write(f"append member {address_name}\n")
-    converted_grp.write("next\n")
+def define_addrgrp(grp_memebers):
+    for grp_name, member in grp_memebers.items():
+        members = ' '.join(member.keys())
+        converted_config.write(f"edit {grp_name}\n")
+        converted_config.write(f"append member {members}\n")
+        converted_config.write("next\n")
 
 
-def policy_writer(policy_id, policy_name, src_int, dst_int, src_addr, dst_addr, port, action, scheduler, count, converted_policies):
-    converted_policies.write(f"edit {policy_id}\n")
-    converted_policies.write(f"set name {policy_name}-P{policy_id}\n")
-    converted_policies.write(f"set srcintf {src_int}\n")
-    converted_policies.write(f"set dstintf {dst_int}\n")
-    converted_policies.write(f"set srcaddr {src_addr}\n")
-    converted_policies.write(f"set dstaddr {dst_addr}\n")
-    # list(filter(lambda item: converted_policies.write(f"set srcaddr {item}\n"), source_addr))
-    # list(filter(lambda item: converted_policies.write(f"set dstaddr {item}\n"), dst_addr))
-    converted_policies.write(f"set action {action}\n")
+def policy_writer(policy_id, policy_name, src_int, dst_int, src_addr, dst_addr, port, action, scheduler, count):
+    converted_config.write(f"edit {policy_id}\n")
+    converted_config.write(f"set name {policy_name}-P{policy_id}\n")
+    converted_config.write(f"set srcintf {src_int}\n")
+    converted_config.write(f"set dstintf {dst_int}\n")
+    converted_config.write(f"set srcaddr {src_addr}\n")
+    converted_config.write(f"set dstaddr {dst_addr}\n")
+    # list(filter(lambda item: converted_config.write(f"set srcaddr {item}\n"), source_addr))
+    # list(filter(lambda item: converted_config.write(f"set dstaddr {item}\n"), dst_addr))
+    converted_config.write(f"set action {action}\n")
     if scheduler:
-        converted_policies.write(f"set schedule {scheduler}\n")
+        converted_config.write(f"set schedule {scheduler}\n")
     else:
-        converted_policies.write("set schedule always\n")
+        converted_config.write("set schedule always\n")
         pass
-    converted_policies.write(f"set service {port}\n")
-    # list(filter(lambda item: converted_policies.write(f"set service {item}\n"), port))
-    # converted_policies.write(f"set service {port}\n")
+    converted_config.write(f"set service {port}\n")
+    # list(filter(lambda item: converted_config.write(f"set service {item}\n"), port))
+    # converted_config.write(f"set service {port}\n")
     if action == "accept":
-        converted_policies.write(
+        converted_config.write(
             f"set utm-status enable\nset ssl-ssh-profile 'certificate-inspection'\nset ips-sensor 'Def_High'\nset logtraffic all\nnext\n")
     else:
-        converted_policies.write(f"set logtraffic all\nnext\n")
+        converted_config.write(f"set logtraffic all\nnext\n")
     count += 1
 
 
@@ -66,12 +77,19 @@ with open("parsed_config.json") as backup_file,\
         open("converted_config.txt", "w") as converted_config:
     data = json.load(backup_file)
     count = 0
+    groups = {}
     converted_config.write("config firewall address\n")
     for pol_id, policy in data.items():
+        p_srcaddr = policy["src_addr"]
+        define_addr(p_srcaddr)
+        p_dstaddr = policy["dst_addr"]
+        define_addr(p_dstaddr)
     converted_config.write("config firewall addrgrp\n")
     for pol_id, policy in data.items():
+        define_addrgrp(groups)
     converted_config.write("config firewall services custom\n")
     for pol_id, policy in data.items():
+        #p_ports = ' '.join(list(map(lambda x: x, policy["pol_proto"].keys())))
     converted_config.write("config firewall policy\n")
     for pol_id, policy in data.items():
         p_name = policy["pol_name"]
@@ -84,4 +102,4 @@ with open("parsed_config.json") as backup_file,\
         p_log = policy["log_action"]
         p_scheduler = "always"
         policy_writer(pol_id, p_name, p_srcint, p_srcint, p_srcaddr,
-                      p_dstaddr, p_ports, p_action, p_scheduler, count, converted_config)
+                      p_dstaddr, p_ports, p_action, p_scheduler, count)
